8099fa9a85bf92b6c9a8d92adab9a8626ad562e3
Rowan Walsh <walshyrowan@gmail.com>
2018-02-02 17:25:21 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ImagePicker } from 'expo';
+
+import Home from './Home';
+
+import {
+	Router,
+	Scene,
+} from 'react-native-router-flux';
+
+
+
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+			  <Scene key = 'root'>
+			    <Scene key='home' component={Home} title='Home'/>
+			    </Scene>
+			</Router>
+		);
+		
+	}
+}
+
+export default App;
