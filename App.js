2c853b33f7dc1c6aaebc3244ecdd6d1a960871e5
Rowan Walsh <walshyrowan@gmail.com>
2018-02-27 23:08:03 +0000

    added: GPS coordinates extracted from exif

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ImagePicker } from 'expo';
 
+
 import Home from './Home';
 
 import {
--- Home.js
+++ Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 import { ImagePicker } from 'expo';
 
 
-export default class App extends React.Component {
+export default class Home extends React.Component {
 	state = {
          image: null,
     };
 
     render() {
-    	let { image } = this.state;
+    	var { image } = this.state;
+    	var var1 = null;
+    	var var2 = null;
+
 
     	return (
     		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -20,23 +23,51 @@ export default class App extends React.Component {
     		    {image &&
     		    	<Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     		</View>
+
     	);
     }
 
+    findCoordinates = (meta) =>{
+    	var exif = meta;
+    	// console.log(exif);
+
+    	this.contains(exif);
+
+    }
+
+    contains = (arr) =>{
+    	// console.log(arr);
+    	lat = arr["GPSLatitude"];
+    	long = arr["GPSLongitude"];
+    	if (lat==null){
+    		console.log('No GPS tag found');
+    		alert('No GPS tag attached');
+    		}else{
+    			if (long==null){
+    				console.log('No GPS tag found');
+    				alert('No GPS tag attached');
+    			}
+    		else{
+    			console.log(lat + ' ' + long);
+    		}
+    	}
+    }
+
+
     _pickImage = async () => {
     	let result = await ImagePicker.launchImageLibraryAsync({
-    		// allowsEditing: true,
-    		// aspect: [4, 3],
+    		allowsEditing: true,
+    		aspect: [4, 3],
     		exif: true,
     	});
 
-    	console.log(result.exif);
-
     	if (!result.cancelled) {
-    		this.setState({ image: result.uri });
+    		this.setState({ image: result.uri});
+    		this.findCoordinates(result.exif);
     	}
 
     };
+
 }
 
 
